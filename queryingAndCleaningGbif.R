f2269165fc677fb58b473779a1c56affecf5c11a
jeremymcwilliams <jeremym@lclark.edu>
2023-02-01 22:31:04 +0000

    added some stuff

--- queryingAndCleaningGbif.R
+++ queryingAndCleaningGbif.R
@@ -2,6 +2,57 @@
 #spocc: interface to SPecies OCCurrence data sources
 
 install.packages("spocc")
-library("spocc")
+install.packages("tidyverse") #includes ggplot
+install.packages("ggspatial")
+library(spocc)
+library(tidyverse)
+library(ggspatial)
+
+#Plan for class:
+
+#Introduce/review functions, using occ as an example.
+
+#Share occ documentation page
+
+#In groups: write a query for Rana draytonii in the United States
+# https://www.rdocumentation.org/packages/spocc/versions/0.1.0/topics/occ
+#from="gbif"
+#limit=""
 
 
+
+
+
+
+
+
+
+
+#might be good:
+# https://paleolimbot.github.io/ggspatial/articles/ggspatial.html#data-frames-with-coordinates
+
+#Bobcat: Lynx rufus
+#Beaver: Castor canadensis
+
+#Ochotona princeps 0?
+# Antilocapra Ord
+#Rana draytonii - has points in Africa - could be good!
+
+
+
+
+
+
+
+query <- occ(query='Rana draytonii', from='gbif', limit=10000)
+
+
+
+rana<-query$gbif$data$Rana_draytonii
+
+
+ggplot(rana, aes(longitude, latitude)) +
+  annotation_map_tile(type = "stamenwatercolor") +
+  geom_spatial_point() 
+ # geom_spatial_label_repel(aes(label = city), box.padding = 1) +
+  #coord_sf(crs = 3995)
